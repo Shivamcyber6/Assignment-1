2dbf5b189ccd7f62d75ed8d08689603dac5cf968
Shivamcyber6 <54810162+Shivamcyber6@users.noreply.github.com>
2019-09-02 23:25:35 +0530

    Add files via upload

---
+++ A1_Q6.java
@@ -0,0 +1,16 @@
+
+public class A1_Q6 {
+
+	public static void main(String[] args) {
+		int year = 10000;
+		if(year% 400 == 0)
+			System.out.println("Yes");
+		else if(year%100==0)
+			System.out.println("no");
+		else if(year % 4 == 0)
+			System.out.print("Yes");
+		
+
+	}
+
+}
