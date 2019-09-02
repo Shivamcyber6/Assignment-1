6ed54ffe71f1c52c7e9d8a6c2792370be202bd47
Shivamcyber6 <54810162+Shivamcyber6@users.noreply.github.com>
2019-09-02 23:28:38 +0530

    Add files via upload

---
+++ A1_Q6c.java
@@ -0,0 +1,20 @@
+
+public class A1_Q6c {
+
+		public static void main(String[] args) {
+			int year=2004;
+		if(year%4==0) {
+			System.out.println("leap");
+		}
+		else if(year%100==0&& year%400==0) {
+			System.out.println("leap");
+		}
+			else
+			{	System.out.println("not");}
+		}
+	
+
+
+	}
+
+
