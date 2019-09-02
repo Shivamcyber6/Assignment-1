2b0342d5b090be7967fc2fb1f2e170c744f041a3
Shivamcyber6 <54810162+Shivamcyber6@users.noreply.github.com>
2019-09-02 23:22:58 +0530

    Add files via upload

---
+++ A3_abcde.java
@@ -0,0 +1,15 @@
+
+public class A3_abcde {
+
+	public static void main(String[] args) {
+		int a=7;
+		int b=9;
+			System.out.println(a&b);
+		System.out.println(a|b);
+		System.out.println(a^b);
+		System.out.println(a<<2);
+		System.out.println(a>>2);
+
+	}
+
+}
