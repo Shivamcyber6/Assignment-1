e4c37fc92caabb06193b32e63a948020f3403beb
Shivamcyber6 <54810162+Shivamcyber6@users.noreply.github.com>
2019-09-02 23:35:46 +0530

    Add files via upload

---
+++ A2_Q2.java
@@ -0,0 +1,20 @@
+
+public class A2_Q2 {
+
+	public static void main(String[] args) {
+		int a = 0,b=1,c =0,d =0;
+		int n = 4000000;
+		while(c<n) {
+			c=a+b;
+			a=b;
+			b=c;
+			if(c%2==0) {
+				d=d+c;
+				System.out.println("d");
+		
+	}
+
+}
+
+	}
+}
