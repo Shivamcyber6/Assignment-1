cae88d1211e85d010cd62d5184ba0d14ee60084b
Shivamcyber6 <54810162+Shivamcyber6@users.noreply.github.com>
2019-09-02 23:23:37 +0530

    Add files via upload

---
+++ Q4.java
@@ -0,0 +1,15 @@
+
+public class Q4 {
+
+	public static void main(String[] args) {
+	int a = 2;
+	int b = 3;
+	int c = 4;
+	int d = 6;
+	String max;
+	max=((a>b && a>c && a>d)?"a":(b>c && b>d)?"b":(c>d)? "c":"d");
+	System.out.println(max);
+
+	}
+
+}
